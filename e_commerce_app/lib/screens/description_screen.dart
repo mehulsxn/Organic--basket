5bab34a64ecf5e80466c1f6c4eb12318a282c224
Mehul Saxena <61407259+mehulsxn@users.noreply.github.com>
2021-07-03 17:54:10 +0530

    description screen updated

--- e_commerce_app/lib/screens/description_screen.dart
+++ e_commerce_app/lib/screens/description_screen.dart
@@ -1,21 +1,104 @@
+import 'package:e_commerce_app/widgets/constant.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DescriptionScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final mq = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.amber,
       body: Column(
         children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child: Container(
+              height: mq.height * 0.35,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(25),
+                    bottomLeft: Radius.circular(25)),
+              ),
+              child: Image.network(
+                'https://images.immediate.co.uk/production/volatile/sites/30/2013/06/Strawberries-in-a-bowl-e53b458-scaled.jpg?quality=90&resize=768%2C574',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: mq.height * 0.02,
+          ),
           Container(
-            height:mq.height*0.1,
-            width:double.infinity,
-            child: Image.network('https://png.pngtree.com/png-clipart/20201226/ourmid/pngtree-delicious-fresh-strawberry-png-image_2645827.jpg'),
+            height: mq.height * 0.2,
+            width: mq.width * 0.95,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: mq.height * 0.001,
+                ),
+                Text('Strawberry',
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: mq.height * 0.04,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      '\$17.00',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '14 Calories',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Icon(Icons.star_border_outlined)
+                  ],
+                ),
+                SizedBox(
+                  height: mq.height * 0.04,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 30,
+                      width: 30,
+                      child: Icon(CupertinoIcons.minus),
+                      decoration: BoxDecoration(
+                          color: KPrimaryColor,
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                    SizedBox(
+                      width: mq.width * 0.1,
+                    ),
+                    Text(
+                      '1 Kg',style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: mq.width * 0.1,
+                    ),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      child: Icon(CupertinoIcons.add),
+                      decoration: BoxDecoration(
+                          color: KPrimaryColor,
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
-
     );
   }
 }
